6262832041b779cc918c437d7ba09fc7cb509b74
bumblebee4105 <luuk@debosmaatjes.nl>
2024-11-29 17:54:30 +0100

    started building frontend

---
+++ frontend/faciliteiten.js
@@ -0,0 +1,21 @@
+const facilities = [
+    {   
+        image: "38.jpg",
+        alt: "Verhuur",
+        title: "Verhuur",
+        text: "Faciliteiten voor het verhuur van:",
+        points: "<a>Elektrische-fietsverhuur</a></li><li><a>Fietsverhuur</a></li><li><a>Skelterverhuur</a></li><li><a>Vergader-/feestfaciliteiten</a>"
+    },
+
+    {
+        image: "38.jpg",
+        alt: "Camping",
+        title: "Camping",
+        text: "Faciliteiten voor de camping:",
+        points: "<a>Afwasplaatsen</a></li><li><a>Babywasgelegenheid op camping</a></li><li><a>Gratis warme douches</a></li><li><a>Huisdieren op camping welkom</a></li><li><a>Stortplaats chemisch toilet</a></li><li><a>Wascabines</a>"
+    }
+];
+
+function faciliteiten(){
+    return facilities;
+}
