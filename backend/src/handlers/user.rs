9412ed6c2f39a27c7a4226f194efd3a24f9203ee
sem pruijs <19208988+sempruijs@users.noreply.github.com>
2024-12-03 11:46:10 +0100

    Improve error message for creating user (#16)

--- backend/src/handlers/user.rs
+++ backend/src/handlers/user.rs
@@ -16,7 +16,7 @@ pub async fn create_user_handler(
 
     match create_user(&pool, user).await {
         Ok(_) => Ok(StatusCode::CREATED),
-        Err(e) => panic!("error while listing users. Error: {}", e),
+        Err(e) => panic!("Error while creating user. {}", e),
     }
 }
 
