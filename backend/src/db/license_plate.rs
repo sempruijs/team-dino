a339e80fc1ea545909e4a308ef65fd2c0dcc2527
sem pruijs <sem@pruijs.net>
2024-12-09 11:29:10 +0100

    Make license_plate_is_valid function

--- backend/src/db/license_plate.rs
+++ backend/src/db/license_plate.rs
@@ -1,6 +1,8 @@
 use sqlx::types::Uuid;
 use sqlx::PgPool;
 
+use crate::db::user::user_is_valid;
+
 pub async fn license_plate_exists(pool: &PgPool, plate: &str) -> Result<bool, sqlx::Error> {
     let exists = sqlx::query_scalar!(
         r#"
@@ -16,6 +18,30 @@ pub async fn license_plate_exists(pool: &PgPool, plate: &str) -> Result<bool, sq
     .await?;
 
     Ok(exists.unwrap())
+}
+
+pub async fn license_plate_is_valid(pool: &PgPool, plate: &str) -> Result<bool, sqlx::Error> {
+    let uuid = get_uuid_from_license_plate(pool, plate).await?;
+    if let Some(user_id) = uuid {
+        return Ok(user_is_valid(pool, user_id).await?);
+    }
+    Ok(false)
+}
+
+pub async fn get_uuid_from_license_plate(
+    pool: &PgPool,
+    plate: &str,
+) -> Result<Option<Uuid>, sqlx::Error> {
+    let result = sqlx::query_scalar!(
+        "SELECT user_id 
+         FROM license_plates 
+         WHERE license_plate = $1",
+        plate
+    )
+    .fetch_optional(pool)
+    .await?;
+
+    Ok(result)
 }
 
 pub async fn create_license_plate(
