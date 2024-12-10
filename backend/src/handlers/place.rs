7d95c86b702a12a436c9b73e9bf14e07d6fc0eee
sem pruijs <sem@pruijs.net>
2024-12-10 11:32:54 +0100

    Create handler for create_place

---
+++ backend/src/handlers/place.rs
@@ -0,0 +1,26 @@
+use crate::db::Place::*;
+use crate::logging::current_time_iso8601;
+use crate::types::Place::*;
+use sqlx::PgPool;
+use warp::http::StatusCode;
+
+pub async fn create_place_handler(
+    place: Place,
+    pool: PgPool,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let now = current_time_iso8601(); // Assuming this function returns the current time as ISO8601
+
+    match create_place(&pool, place).await {
+        Ok(_) => {
+            println!(
+                "New place created: {} at: {}",
+                 place.place_id, now
+            );
+            Ok(StatusCode::CREATED)
+        },
+        Err(e) => {
+            panic!("Error while creating place: {}", e);
+        }
+    }
+}
+}
