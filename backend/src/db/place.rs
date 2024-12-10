001c1dfc1c7a449dbe1cb3b039039534a2b630a9
sem pruijs <sem@pruijs.net>
2024-12-10 11:24:13 +0100

    create list places function

--- backend/src/db/place.rs
+++ backend/src/db/place.rs
@@ -14,20 +14,16 @@ pub async fn create_place(pool: &PgPool, place: Place) -> Result<(), sqlx::Error
     Ok(())
 }
 
-// pub async fn get_tickets(pool: &PgPool, user_id: Uuid) -> Result<Vec<Ticket>, sqlx::Error> {
-//     // Query all tickets associated with the user
-//     let tickets = sqlx::query_as!(
-//         Ticket,
-//         r#"
-//         SELECT ticket_id, user_id, start_date, end_date, house_number
-//         FROM tickets
-//         WHERE user_id = $1
-//         "#,
-//         user_id
-//     )
-//     .fetch_all(pool)
-//     .await?;
+pub async fn get_places(pool: &PgPool) -> Result<Vec<Place>, sqlx::Error> {
+    let places = sqlx::query_as!(
+        Place,
+        r#"
+        SELECT place_id, house_number
+        FROM places
+        "#
+    )
+    .fetch_all(pool)
+    .await?;
 
-//     // Check if any ticket is valid
-//     Ok(tickets)
-// }
+    Ok(places)
+}
