0a037851135d3ed067409a447044fe5c3c93bf94
sem pruijs <sem@pruijs.net>
2024-12-16 14:22:13 +0100

    Write avaiable places function

--- backend/src/db/place.rs
+++ backend/src/db/place.rs
@@ -1,4 +1,5 @@
 use crate::types::place::*;
+use chrono::NaiveDate;
 use sqlx::types::Uuid;
 use sqlx::PgPool;
 
@@ -40,4 +41,35 @@ pub async fn delete_place(pool: &PgPool, place_id: Uuid) -> Result<u64, sqlx::Er
     .await?;
 
     Ok(result.rows_affected())
+}
+
+pub async fn get_available_places(
+    pool: &PgPool,
+    start_date: NaiveDate,
+    end_date: NaiveDate,
+) -> Result<Vec<Place>, sqlx::Error> {
+    // Query to fetch available places
+    let places = sqlx::query_as!(
+        Place,
+        r#"
+        SELECT places.place_id, places.house_number
+        FROM places
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM tickets
+            WHERE tickets.place_id = places.place_id
+            AND (
+                (tickets.start_date <= $2 AND tickets.end_date >= $1)
+                OR (tickets.start_date <= $1 AND tickets.end_date >= $2)
+                OR (tickets.start_date >= $1 AND tickets.end_date <= $2)
+            )
+        )
+        "#,
+        start_date,
+        end_date
+    )
+    .fetch_all(pool)
+    .await?;
+
+    Ok(places)
 }
