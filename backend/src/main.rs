6d8ccf22d2d7ec1443596ab83d71f12f611b15ad
sem pruijs <sem@pruijs.net>
2025-01-11 18:41:20 +0100

    Make hello world for rocket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,32 +1,22 @@
-use crate::controller::actix_routes::serve_actix_routes;
-use dotenv::dotenv;
-use sqlx::postgres::PgPoolOptions;
-use sqlx::PgPool;
-use std::env;
+#[macro_use]
+extern crate rocket;
 
-// Import different modules that make up the backend
-pub mod controller;
-pub mod docs;
-pub mod repository;
-pub mod service;
-pub mod traits;
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
 
-// types that are used accross the modules.
-pub mod types;
+#[get("/hello")]
+fn hello() -> &'static str {
+    "Hello, saas!"
+}
 
-// Main gets executed on startup.
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    // Read the database path from enviousment variables.
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let secret_key = env::var("DINO_SECRET_KEY").expect("DINO_SECRET_KEY must be set");
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/", routes![hello, index])
+        .launch()
+        .await?;
 
-    // thread pool that executes sql queries.
-    let pool = PgPoolOptions::new().connect(&database_url).await.unwrap();
-
-    // Expose all api's
-    // serve_routes(pool, secret_key).await;
-
-    serve_actix_routes(pool, secret_key).await
+    Ok(())
 }
