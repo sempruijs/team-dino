f9c721790ea45014ec8e9e7df6ef037384c98f32
sem pruijs <sem@pruijs.net>
2025-01-13 00:22:21 +0100

    Write get available places function in repository layer

--- backend/src/repository/place.rs
+++ backend/src/repository/place.rs
@@ -10,6 +10,12 @@ pub trait PlaceRepository: Send + Sync {
 
     async fn get_places(&self) -> Result<Vec<Place>, sqlx::Error>;
 
+    async fn get_available_places(
+        &self,
+        start_date: NaiveDate,
+        end_date: NaiveDate,
+    ) -> Result<Vec<Place>, sqlx::Error>;
+
     // async fn delete(&self, place_id: Uuid) -> Result<u64, sqlx::Error>;
 }
 
@@ -49,6 +55,37 @@ impl PlaceRepository for PlaceRepositoryImpl {
         .execute(&self.pool)
         .await?;
         Ok(())
+    }
+
+    async fn get_available_places(
+        &self,
+        start_date: NaiveDate,
+        end_date: NaiveDate,
+    ) -> Result<Vec<Place>, sqlx::Error> {
+        // Query to fetch available places
+        let places = sqlx::query_as!(
+            Place,
+            r#"
+                SELECT places.place_id, places.house_number
+                FROM places
+                WHERE NOT EXISTS (
+                    SELECT 1
+                    FROM tickets
+                    WHERE tickets.place_id = places.place_id
+                    AND (
+                        (tickets.start_date <= $2 AND tickets.end_date >= $1)
+                        OR (tickets.start_date <= $1 AND tickets.end_date >= $2)
+                        OR (tickets.start_date >= $1 AND tickets.end_date <= $2)
+                    )
+                )
+                "#,
+            start_date,
+            end_date
+        )
+        .fetch_all(&self.pool)
+        .await?;
+
+        Ok(places)
     }
 }
 
