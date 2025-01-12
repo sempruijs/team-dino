64d0b6699466c894eb1de078598992f40722befe
sem pruijs <sem@pruijs.net>
2025-01-12 20:38:10 +0100

    Implement from uuid for service layer

--- backend/src/service/user.rs
+++ backend/src/service/user.rs
@@ -3,11 +3,14 @@ use crate::domain::User;
 use crate::repository::user::*;
 use crate::traits::*;
 use rocket::async_trait;
+use uuid::Uuid;
 
 #[async_trait]
 pub trait UserService: Send + Sync {
     // should be a service error.
     async fn create(&self, user: User) -> Result<(), sqlx::Error>;
+
+    async fn from_uuid(&self, user_id: Uuid) -> Result<Option<User>, sqlx::Error>;
 }
 
 pub struct UserServiceImpl<T: UserRepository> {
@@ -49,5 +52,9 @@ impl<R: UserRepository> UserService for UserServiceImpl<R> {
     // should be a service error.
     async fn create(&self, user: User) -> Result<(), sqlx::Error> {
         self.user_repository.create(user).await
+    }
+
+    async fn from_uuid(&self, user_id: Uuid) -> Result<Option<User>, sqlx::Error> {
+        self.user_repository.from_uuid(user_id).await
     }
 }
