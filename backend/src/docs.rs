7566bc8545e0d9b872912d73ba5d442caf8ee113
sem pruijs <sem@pruijs.net>
2025-01-13 17:39:58 +0100

    Integrate login documentation

--- backend/src/docs.rs
+++ backend/src/docs.rs
@@ -1,10 +1,11 @@
+use crate::controller::authentication::*;
 use crate::controller::place::*;
 use crate::controller::user::*;
 use utoipa::OpenApi;
 
 #[derive(OpenApi)]
 #[openapi(
-    paths(create_user, get_user, get_places ), // Add authenticate_user_handler here
+    paths(create_user, get_user, get_places, login, ), // Add authenticate_user_handler here
     components(
         schemas(CreateUserRequest) // Add the schemas
     )
